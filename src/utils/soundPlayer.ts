24a40f46270ecc7353f60413ae075f6ad1366722
Gariqz <ghassanariq7@gmail.com>
2025-05-13 11:34:15 +0700

    feat : added utils

--- src/utils/soundPlayer.ts
+++ src/utils/soundPlayer.ts
@@ -6,11 +6,16 @@ if (typeof window !== "undefined") {
     spin: new Audio('/assets/sounds/wheel.wav'),
     win: new Audio('/assets/sounds/win.wav'),
     lose: new Audio('/assets/sounds/lose.mp3'),
+    doorOpen: new Audio('/assets/sounds/doorOpened.mp3'),
+    doorLocked: new Audio('/assets/sounds/doorLocked.mp3'),
+    bgm: new Audio('/assets/sounds/bgm.mp3')
   };
+  sounds.bgm.loop = true;
+  sounds.bgm.volume = 0.1;
 }
 
 // function to play sound based on the key provided
-export const playSound = (key: 'spin' | 'win' | 'lose') => {
+export const playSound = (key: 'spin' | 'win' | 'lose' | 'doorOpen' | 'doorLocked' | 'bgm') => {
   if (typeof window === "undefined") return; // Pastikan kode hanya dijalankan di browser
   const sound = sounds[key];
   if (!sound) return;
